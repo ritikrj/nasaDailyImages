58a6c8bd3e2a241bcb3975a56c0414daac8042c2
ritik <ritikranjan825@gmail.com>
2022-04-05 08:50:39 +0530

    UI: state persistence on configuration change

--- app/src/main/java/com/rk/nasadailyimages/UI/HomeActivity.kt
+++ app/src/main/java/com/rk/nasadailyimages/UI/HomeActivity.kt
@@ -2,22 +2,29 @@ package com.rk.nasadailyimages.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import com.rk.nasadailyimages.Common.Constants
 import com.rk.nasadailyimages.R
 import com.rk.nasadailyimages.UI.screens.ImageOfDayScreen.ImageOfDayFrag
 
 class HomeActivity : AppCompatActivity() {
-
+    var currFragment = favouriteImagesFragment::class.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        val tr = supportFragmentManager.beginTransaction()
-        supportFragmentManager.findFragmentById(R.id.container)?.let { tr.remove(it) }
-        tr.add(R.id.container, ImageOfDayFrag())
-        tr.commit()
+         currFragment = savedInstanceState?.getString(Constants.CURR_FRAG_KEY)
+       if(currFragment == favouriteImagesFragment::class.simpleName){
+           showFavFragment()
+       }else{
+           showImageOfDayFragment()
+       }
+//        val tr = supportFragmentManager.beginTransaction()
+//        supportFragmentManager.findFragmentById(R.id.container)?.let { tr.remove(it) }
+//        tr.add(R.id.container, ImageOfDayFrag())
+//        tr.commit()
 
     }
 
@@ -40,8 +47,9 @@ class HomeActivity : AppCompatActivity() {
     fun showFavFragment(){
         val tr = supportFragmentManager.beginTransaction()
         supportFragmentManager.findFragmentById(R.id.container)?.let { tr.remove(it) }
-        tr.add(R.id.container,ImageOfTheDayFragment.newInstance())
+        tr.add(R.id.container,favouriteImagesFragment.newInstance())
         tr.commit()
+        currFragment = favouriteImagesFragment::class.simpleName
     }
 
     fun showImageOfDayFragment(){
@@ -49,6 +57,12 @@ class HomeActivity : AppCompatActivity() {
         supportFragmentManager.findFragmentById(R.id.container)?.let { tr.remove(it) }
         tr.add(R.id.container, ImageOfDayFrag())
         tr.commit()
+        currFragment = ImageOfDayFrag::class.simpleName
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(Constants.CURR_FRAG_KEY, currFragment)
     }
 
 
